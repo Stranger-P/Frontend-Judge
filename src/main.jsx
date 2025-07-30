c98420c53a5b42fda53d14b37537b3a3dd5b5e23
Priyanshu Chouhan <chouhanpriyanshu98933@gmail.com>
2025-07-31 00:28:29 +0530

    axios

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import { Provider } from 'react-redux';
 import { store } from './store/index.js';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'; 
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -26,7 +25,6 @@ createRoot(document.getElementById('root')).render(
           <App />
         </BrowserRouter>
       </Provider>
-      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
     </QueryClientProvider>
   </StrictMode>
 );
